c624ae76287c4b769c2f667786ffecc839b954da
Lucas <lucas.desenvolvedor@hotmail.com>
2023-11-13 17:01:56 -0300

    Metodos GET e SET

---
+++ get_set/Aula04.java
@@ -0,0 +1,10 @@
+package get_set;
+
+public class Aula04 {
+    public static void main(String[] args) {
+        
+        Caneta c1 = new Caneta("Nice", "Amarela", 0.6f);
+
+        c1.status();
+    }
+}
